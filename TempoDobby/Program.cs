aa28a48975a9d3b9b3cb32f8ef3b12eec44eca4a
Jean Leonardo Soares de Moraes <jean.jlsm2@gmail.com>
2021-09-21 05:08:30 -0300

    Add project files.

---
+++ TempoDobby/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TempoDobby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line1 = Console.ReadLine().Split();
+            string[] line2 = Console.ReadLine().Split();
+
+            int finalExpediente = int.Parse(line1[0]);
+
+            int presente1 = int.Parse(line2[0]);
+            int presente2 = int.Parse(line2[1]);
+
+            if ((presente1 + presente2) <= finalExpediente)
+            {
+                Console.WriteLine("Farei hoje!");
+            }
+            else
+            {
+                Console.WriteLine("Deixa para amanha!");
+            }
+        }
+    }
+}
